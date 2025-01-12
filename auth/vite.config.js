592ee381c0e17dd4c6be7435b45935c8e8bdb4e6
Igor Alyanoy <igoralyanoy@gmail.com>
2025-01-12 13:45:22 +0200

    feat: add module federation to vite configs

--- auth/vite.config.js
+++ auth/vite.config.js
@@ -1,10 +1,18 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import federation from "@originjs/vite-plugin-federation";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    federation({
+      name: "auth",
+      filename: "authRemoteEntry.js",
+      exposes: {
+        "./index": "./src/bootstrap.js",
+      },
+    }),
   ],
   build: {
     modulePreload: false,
--- task-list/vite.config.ts
+++ task-list/vite.config.ts
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import federation from '@originjs/vite-plugin-federation';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    federation({
+      name: 'task-list',
+      filename: 'taskListRemoteEntry.js',
+      exposes: {
+        './index': './src/App.tsx',
+      },
+      shared: ['react', 'react-dom'],
+    }),
   ],
   build: {
     modulePreload: false,
