3a675627b0313a919061b7c5946e39af7a63b7b3
Igor Alyanoy <igoralyanoy@gmail.com>
2025-01-15 21:50:16 +0200

    feat: add auth

--- auth/src/bootstrap.ts
+++ auth/src/bootstrap.ts
@@ -1,8 +1,9 @@
 import { createApp } from 'vue';
 import App from './App.vue';
+import './assets/index.css';
 
-const mount = (el) => {
-  const app = createApp(App);
+const mount = (el: Element, props?: { onNavigate: any; supabase: any }) => {
+  const app = createApp(App, props);
   app.mount(el);
 };
 
